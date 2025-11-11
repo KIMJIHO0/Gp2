785e891c78890ae39eff012075541e0765da3507
E.Koinē <elruien0604@gmail.com>
2025-11-12 04:50:42 +0900

    AppPanel->AppPage 이름 변경, UIComponentTest 추가

--- src/ui_kit/MainFrame.java
+++ src/ui_kit/MainFrame.java
@@ -11,13 +11,13 @@ import config.Constants;
 
 /**
  * 애플리케이션의 메인 셸(Shell)
- * 주로 CardLayout을 사용하여 AppPanel들을 교체하는 페이지 교체 및 관련 이벤트 관리 담당
+ * 주로 CardLayout을 사용하여 AppPage들을 교체하는 페이지 교체 및 관련 이벤트 관리 담당
  */
 public class MainFrame extends JFrame {
     private final CardLayout cardLayout;
-    private final JPanel mainPanel; // AppPanel들이 올라갈 컨테이너
-    private final Map<String, AppPanel> pages = new HashMap<>();
-    private AppPanel currentPage = null;
+    private final JPanel mainPanel; // AppPage들이 올라갈 컨테이너
+    private final Map<String, AppPage> pages = new HashMap<>();
+    private AppPage currentPage = null;
 
     public MainFrame() {
         setTitle(Constants.UI_TITLE);                     // 화면 이름
@@ -43,9 +43,9 @@ public class MainFrame extends JFrame {
     /**
      * Main.java 호출용
      * 각 페이지(패널)을 메인 프레임에 등록
-     * @param panel 등록할 AppPanel 인스턴스
+     * @param panel 등록할 AppPage 인스턴스
      */
-    public void addPage(AppPanel panel) {
+    public void addPage(AppPage panel) {
         String pageId = panel.getPageId();
         if (pageId == null || pageId.isEmpty())
             throw new IllegalArgumentException("page id는 null 또는 빈 문자열이 아니어야 합니다.");
